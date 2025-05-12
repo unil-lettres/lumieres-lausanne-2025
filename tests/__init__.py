adc7972a5fbdb2f41f1d02e1c671a9e02950d6a4
Xavier Beheydt <xavier.beheydt@gmail.com>
2025-05-12 09:06:13 +0000

    Add testing support - Close #6

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+"""
+Unit tests for Lumieres Lausanne Project.
+"""
---
+++ tests/test_sample.py
@@ -0,0 +1,20 @@
+"""
+Tests sample.
+"""
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    import requests
+
+    return requests.get("https://localhost:8080")
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    from bs4 import BeautifulSoup
+
+    assert "Unil" in BeautifulSoup(response.content).title.string
