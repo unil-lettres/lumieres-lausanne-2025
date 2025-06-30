c00b8415563e1865a89109577d9c43947a36c9f6
Xavier Beheydt <xavier.beheydt@gmail.com>
2025-06-30 08:06:42 +0000

    fix(models): include Depot model in __all__ exports in __init__.py

--- app/fiches/models/documents/__init__.py
+++ app/fiches/models/documents/__init__.py
@@ -12,6 +12,7 @@ from .document import (
     NoteBiblio,
     NoteTranscription,
     DocumentLanguage,
+    Depot,
 )
 
 __all__ = [
@@ -28,4 +29,5 @@ __all__ = [
     "NoteBiblio",
     "NoteTranscription",
     "DocumentLanguage",
+    "Depot",
 ]
