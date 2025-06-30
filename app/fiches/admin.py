19bfdbdde3ca40453f2139ee8f25d9e03fcd7a0d
Xavier Beheydt <xavier.beheydt@gmail.com>
2025-06-30 08:04:25 +0000

    refactor(admin): enhance code organization and readability in admin.py

--- app/fiches/admin.py
+++ app/fiches/admin.py
@@ -4,10 +4,13 @@ from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.models import User, Group
-from django.urls import reverse as reverse_url
+from django.urls import reverse as reverse_url, path
 from django.forms import ModelForm, TextInput, CharField
 from django.utils.translation import gettext_lazy as _
 from django.utils.html import format_html
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 import os
 
 from django.contrib.contenttypes.admin import GenericStackedInline
@@ -44,6 +47,7 @@ from fiches.models import (
     ActivityLog,
     Document,
     Relation,
+    Depot,
 )
 from fiches.forms import ObjectCollectionForm, BiblioForm
 
@@ -55,9 +59,30 @@ from ckeditor.widgets import CKEditorWidget  # Ensure CKEditor is up-to-date
 
 
 class FichesAdminSite(AdminSite):
-    site_header = "Fiches Administration"
-    site_title = "Fiches Admin"
-    index_title = "Welcome to the Fiches Admin"
+    """
+    Custom admin site for Lumières.Lausanne application.
+    
+    Provides a branded administration interface with University of Lausanne styling.
+    """
+    site_header = "Administration Lumières.Lausanne"
+    site_title = "Lumières.Lausanne Admin"
+    index_title = "Administration du site"
+    site_url = "/"
+    
+    def each_context(self, request):
+        """
+        Return a dictionary of variables to put in the template context for
+        every page in the admin site.
+        """
+        context = super().each_context(request)
+        context.update({
+            'site_header': self.site_header,
+            'site_title': self.site_title,
+            'index_title': self.index_title,
+            'site_url': self.site_url,
+            'has_permission': request.user.is_active and request.user.is_staff,
+        })
+        return context
 
 
 fiches_admin = FichesAdminSite(name="fiches_admin")
@@ -86,6 +111,16 @@ class DocumentInline(GenericStackedInline):
     extra = 0
 
 
+# Add Biography inline for Person admin
+class BiographyInline(admin.StackedInline):
+    """Inline admin for Biography model."""
+    
+    model = Biography
+    extra = 0
+    fields = ('public_functions', 'birth_place', 'birth_date', 'death_place', 'death_date', 'valid')
+    classes = ('collapse',)
+
+
 # -----------------------------------------------------------------------------
 # ModelAdmin Classes
 # -----------------------------------------------------------------------------
@@ -97,29 +132,55 @@ class ContributionTypeAdmin(admin.ModelAdmin):
     list_filter = ("type",)
 
 
-@admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "field_of_research", "shib_uniqueID")
-    search_fields = ("user__username", "field_of_research", "shib_uniqueID")
+    """Admin interface for UserProfile model."""
+    
+    list_display = ("user",)
+    search_fields = ("user__username",)
     list_filter = ("user__is_active", "user__is_staff")
     ordering = ("user__username",)
     readonly_fields = ("user",)
 
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    """Admin interface for Person model (reverted to basic version)."""
+    list_display = ("name", "modern", "may_have_biography")
+    list_display_links = ("name",)
+    list_filter = ("modern", "may_have_biography")
     search_fields = ("name",)
+    ordering = ("name",)
 
 
 class PrimaryKeywordAdmin(admin.ModelAdmin):
-    list_display = ("word",)
+    """Admin interface for PrimaryKeyword model."""
+    
+    list_display = ("word", "secondary_keywords_count")
+    list_display_links = ("word",)
     search_fields = ("word",)
+    ordering = ("word",)
+    
+    @admin.display(description="Mots clés secondaires")
+    def secondary_keywords_count(self, obj):
+        """Display count of related secondary keywords."""
+        count = obj.secondary_keywords.count()
+        if count > 0:
+            return format_html(
+                '<a href="{}?primary_keyword__id__exact={}">{} mot(s) clé(s)</a>',
+                reverse_url("fiches_admin:fiches_secondarykeyword_changelist"),
+                obj.id,
+                count
+            )
+        return "Aucun"
 
 
 class SecondaryKeywordAdmin(admin.ModelAdmin):
+    """Admin interface for SecondaryKeyword model."""
+    
     list_display = ("word", "primary_keyword")
+    list_display_links = ("word",)
     search_fields = ("word", "primary_keyword__word")
     list_filter = ("primary_keyword",)
+    ordering = ("primary_keyword__word", "word")
 
 
 class SocietyAdmin(admin.ModelAdmin):
@@ -141,19 +202,39 @@ class DocumentTypeAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
+class DocumentLanguageAdmin(admin.ModelAdmin):
+    """Admin interface for DocumentLanguage model."""
+    
+    list_display = ("name", "code", "ordering")
+    list_display_links = ("name",)
+    list_editable = ("ordering",)
+    ordering = ("ordering", "name")
+    search_fields = ("name", "code")
+
+
+class DepotAdmin(admin.ModelAdmin):
+    """Admin interface for Depot (Repository Location) model."""
+    
+    list_display = ("name",)
+    list_display_links = ("name",)
+    ordering = ("name",)
+    search_fields = ("name",)
+
+
 class DocumentFileAdmin(admin.ModelAdmin):
-    list_display = ("file",)
+    list_display = ("file", "file_link")
     search_fields = ("file__name",)
 
+    @admin.display(description="File Link")
     def file_link(self, obj):
+        """Generate a clickable link for the file."""
         if obj.file and obj.file.name:
-            return format_html('<a href="{}">{}</a>', obj.get_absolute_url(), os.path.basename(obj.file.name))
+            return format_html(
+                '<a href="{}" target="_blank">{}</a>',
+                obj.get_absolute_url(),
+                os.path.basename(obj.file.name)
+            )
         return "No file available"
-
-    file_link.short_description = "File Link"
-
-    def view_on_site(self, obj):
-        return obj.get_absolute_url()
 
 
 class FindingAdmin(admin.ModelAdmin):
@@ -198,10 +279,6 @@ class PlaceViewAdmin(admin.ModelAdmin):
     ordering = ("place_name",)
 
 
-from django.contrib import admin
-from .models import JournaltitleView
-
-
 @admin.register(JournaltitleView)
 class JournaltitleViewAdmin(admin.ModelAdmin):
     list_display = ("journal_title",)
@@ -209,13 +286,19 @@ class JournaltitleViewAdmin(admin.ModelAdmin):
 
 
 class UserGroupAdmin(admin.ModelAdmin):
-    list_display = ("name", "description")
+    """Admin interface for UserGroup model."""
+    
+    list_display = ("name", "sort")
     search_fields = ("name",)
 
 
 class NationalityAdmin(admin.ModelAdmin):
+    """Admin interface for Nationality model."""
+    
     list_display = ("name",)
+    list_display_links = ("name",)
     search_fields = ("name",)
+    ordering = ("name",)
 
 
 class RelationTypeAdmin(admin.ModelAdmin):
@@ -300,13 +383,12 @@ class FichesUserAdmin(UserAdmin):
     list_display = ("username", "email", "first_name", "last_name", "is_staff", "userActivities")
     list_per_page = 25
 
+    @admin.display(description="Suivi de l'activité")
     def userActivities(self, obj):
         return format_html(
             '<a href="{}">[ suivi ]</a>',
             reverse_url("fiches_admin:fiches_activitylog_changelist") + f"?user__id__exact={obj.id}",
         )
-
-    userActivities.short_description = "Suivi de l'activité"
 
 
 class FichesGroupAdmin(GroupAdmin):
@@ -322,6 +404,14 @@ fiches_admin.register(ActivityLog, ActivityLogAdmin)
 fiches_admin.register(News, NewsAdmin)
 fiches_admin.register(FreeContent, FreeContentAdmin)
 fiches_admin.register(DocumentFile, DocumentFileAdmin)
+fiches_admin.register(UserGroup, UserGroupAdmin)
+fiches_admin.register(UserProfile, UserProfileAdmin)
+fiches_admin.register(DocumentLanguage, DocumentLanguageAdmin)
+fiches_admin.register(Depot, DepotAdmin)
+fiches_admin.register(PrimaryKeyword, PrimaryKeywordAdmin)
+fiches_admin.register(SecondaryKeyword, SecondaryKeywordAdmin)
+fiches_admin.register(Nationality, NationalityAdmin)
+fiches_admin.register(Person, PersonAdmin)
 
 # fiches_admin.register(ContributionType, ContributionTypeAdmin)
 # fiches_admin.register(UserProfile, UserProfileAdmin)
@@ -335,7 +425,6 @@ fiches_admin.register(DocumentFile, DocumentFileAdmin)
 # fiches_admin.register(Manuscript, ManuscriptAdmin)          # Correctly registering Manuscript
 # fiches_admin.register(Transcription, TranscriptionAdmin)
 # fiches_admin.register(DocumentType, DocumentTypeAdmin)
-# fiches_admin.register(DocumentLanguage)                    # Assuming default admin
 # fiches_admin.register(Finding, FindingAdmin)
 # fiches_admin.register(Image, ImageAdmin)                    # Corrected registration
 # fiches_admin.register(ObjectCollection, ObjectCollectionAdmin)
