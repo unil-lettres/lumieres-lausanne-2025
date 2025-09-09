e69042677d466eb84d5717f449feb32ef4191f8f
Xavier Beheydt <xavier.beheydt@gmail.com>
2025-09-09 13:49:24 +0000

    Fix ([#24](https://github.com/unil-lettres/lumieres-lausanne-2025/issues/24) [#25](https://github.com/unil-lettres/lumieres-lausanne-2025/issues/25) [#26](https://github.com/unil-lettres/lumieres-lausanne-2025/issues/26) [#27](https://github.com/unil-lettres/lumieres-lausanne-2025/issues/27) [#28](https://github.com/unil-lettres/lumieres-lausanne-2025/issues/28) [#29](https://github.com/unil-lettres/lumieres-lausanne-2025/issues/29) [#30](https://github.com/unil-lettres/lumieres-lausanne-2025/issues/30)): Fix contribution type - closes

--- app/fiches/forms.py
+++ app/fiches/forms.py
@@ -475,9 +475,15 @@ class ContributionDocForm(forms.ModelForm):
             except (ValueError, Person.DoesNotExist):
                 raise forms.ValidationError("Cette personne est introuvable dans la base.")
         else:
-            name = raw_value.strip()
-            person, _ = Person.objects.get_or_create(name=name)
-            return person
+                # No pipe => user typed something but no ID
+                # Try to find by name, else create
+                name = raw_value.strip()
+                try:
+                    person, created = Person.objects.get_or_create(pk=int(name))
+                    return None
+                except ValueError:
+                    person, created = Person.objects.get_or_create(name=name)
+                    return person
 
     #
     # 3) Optionally skip entire row if no person was set
